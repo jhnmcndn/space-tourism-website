953355a49ae3289e58e483e8e33ff393ffca3d6c
John Marco Condino <johnmcondino@gmail.com>
2022-12-03 01:19:52 +0800

    asdh

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import style from "./App.module.scss";
 
 function App() {
-  return <div className={style.App}>App</div>;
+  return (
+    <div className={style.App}>
+      <div className={style.container}><h1>This is a test</h1></div>
+    </div>
+  );
 }
 
 export default App;
